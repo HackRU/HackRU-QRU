f9f6e75bb9ef072b6510e6f7f2fd0a1b398a162b
BenYang1998 <blobersthethird@gmail.com>
2018-04-18 18:53:43 -0400

    final

--- label-maker-backend/fuxiewuxie.py
+++ label-maker-backend/fuxiewuxie.py
@@ -1,83 +1,78 @@
 import requests
-import json
-from pymongo import MongoClient
-
+#import json
 import sys
 from os import path
-from datetime import datetime, timedelta
 from win32com.client import Dispatch
-from tkinter import Tk
-import tkinter.messagebox as mbox
+import xml.etree.ElementTree as ET
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/<email>')
+def print_label(email):
+    #email = input('Input email address')
+    #email = 'yang.benjamin1998@gmail.com'
+    #password = 'defacto'
+
+    curdir = None
+    if getattr(sys, 'frozen', False):
+        # frozen
+        curdir = path.dirname(sys.executable)
+    else:
+        # unfrozen
+        curdir = path.dirname(path.abspath(__file__))
 
 
-#email = input('Input email address')
-email = 'testing@hackru.org'
-password = 'defacto'
+    mylabel = path.join(curdir,'betterLabel.label')
 
-curdir = None
-if getattr(sys, 'frozen', False):
-	# frozen
-	curdir = path.dirname(sys.executable)
-else:
-	# unfrozen
-	curdir = path.dirname(path.abspath(__file__))
+    print(mylabel)
 
+    url = 'https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/mlhtest'
 
-mylabel = path.join(curdir,'betterLabel.label')
+    #fake_user = {
+    #	'email': email,
+    #	'password': password,
+    #}
+    #auth = requests.post(url + '/create', json=fake_user)
+    #token = auth.json()['body']
 
-print(mylabel)
+    #read = requests.post(url + '/read', json=(query_d))
+    #print(read.text)
 
-url = 'https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/mlhtest'
-
-fake_user = {
-	'email': email,
-	'password': password,
-}
-auth = requests.post(url + '/create', json=fake_user)
-token = auth.json()['body']
-
-query_d = {
-            'email': email,
-            #'token': token,
-            'query': {'email': email}
+    query_d = {
+        'email' : email,
+        'query' : {'email': email}
     }
 
-read = requests.post(url + '/read', json=(query_d))
-print(read.text)
+    qrimage = requests.post(url + '/qr', json=(query_d))
+    #print(qrimage.json()['body'][22:])
 
 
-import xml.etree.ElementTree as ET
-tree = ET.parse(mylabel)
-root = tree.getroot()
-#print(root[5][0][14][0][0].text)
-first_name = root[5][0][14][0][0]
-qr_text = root[6][0][9]
-last_name = root[7][0][14][0][0]
+    tree = ET.parse(mylabel)
+    root = tree.getroot()
+    first_name = root[5][0][14][0][0]
+    last_name = root[6][0][14][0][0]
+    qr_text = root[7][0][9]
 
 
-first_name.text = 'Billy Bob'
-last_name.text = 'hehehe xd'
-qr_text.text = 'wtf'
+    first_name.text = email
+    last_name.text = email
+    qr_text.text = qrimage.json()['body'][22:]
 
-tree.write(mylabel)
-#print(root)
+    tree.write(mylabel)
+    #print(root)
 
-#client = MongoClient(url) #DB_URI
-#print(client)
-#db = client['camelot-test']
-#db.authenticate(email, password)  #DB_USER = , DB_PASS =
-#test = db['test']
-#u = test.find_one({'email': email})
-#print(u)
-#test.delete_one({'email': email})
-
-labelCom = Dispatch('Dymo.DymoAddIn')
-labelText = Dispatch('Dymo.DymoLabels')
-isOpen = labelCom.Open(mylabel)
-selectPrinter = 'DYMO LabelWriter 450'
-labelCom.SelectPrinter(selectPrinter)
+    print(mylabel)
 
 
-labelCom.StartPrintJob()
-labelCom.Print(1,False)
-labelCom.EndPrintJob()
+    labelCom = Dispatch('Dymo.DymoAddIn')
+    labelText = Dispatch('Dymo.DymoLabels')
+    isOpen = labelCom.Open(mylabel)
+    selectPrinter = 'LabelWriter 450 Turbo'
+    labelCom.SelectPrinter(selectPrinter)
+
+
+    labelCom.StartPrintJob()
+    labelCom.Print(1,False)
+    labelCom.EndPrintJob()
+    return 'hello world'
